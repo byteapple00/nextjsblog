88f894fdb8bf1e07d8a656d729d7677620540cd5
Zenn <skyz3956@gmail.com>
2023-03-27 09:38:48 +0700

    update css style

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,32 +9,36 @@ export default function RootLayout({
 }) {
   const header = (
     <header>
-      <div>
+      <div className="text-center bg-slate-800 p-8 my-6 rounded-md">
         <Link href="/">
-        <h1 className="text-3xl font-bold">
-        teknary log
-        </h1>
+          <h1 className="text-3xl text-white font-bold">noodle_nonsense's blog 💻</h1>
         </Link>
-        <p>Welcome</p>
-        <br/>s
+        <p className="text-slate-300">🖖 Welcome to my blog!</p>
+        <br/>
       </div>
     </header>
   );
   const footer = (
     <footer>
-      <div>
-        <h3>Develop by Tien</h3>
+      <div className="border-t border-slate-500 mt-6 py-6 text-center text-slate-500">
+        <br/>
+        <h3>Develop by noodle_nonsense</h3>
       </div>
     </footer>
   );
+
   return (
     <html>
       <head />
-      <body>
+      <body >
+        <div className="mx-auto border border-red-500 max-w-2xl px-6">
         {header}
         {children}
         {footer}
-        </body>
+        </div>
+      </body>
     </html>
   );
 }
+
+
--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -24,10 +24,16 @@ const PostPage = (props: any) => {
     const slug = props.params.slug;
     const post = getPostContent(slug);
     return (
-        <div> 
-            <h1>{post.data.title}</h1> 
-            <Markdown>{post.content}</Markdown>
+      <div>
+        <div className="my-12 text-center">
+          <h1 className="text-2xl text-slate-600 ">{post.data.title}</h1>
+          <p className="text-slate-400 mt-2">{post.data.date}</p>
         </div>
+  
+        <article className="prose">
+          <Markdown>{post.content}</Markdown>
+        </article>
+      </div>
     );
 };
 
