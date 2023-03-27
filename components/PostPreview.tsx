ea438d14a8c0fad218e4ede73cac71b37a8eccb4
Zenn <skyz3956@gmail.com>
2023-03-27 09:38:48 +0700

    update css style

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -3,9 +3,9 @@ import { PostMetadata } from './PostMetadata';
 
 export const PostPreview = (props: PostMetadata) => {
     return (
-        <div>
+        <div className='border border-violet-200 p-4 rounded-md shadow-md bg-white'>
             <Link href={`/posts/${props.slug}`}>
-            <h2>{props.title}</h2>
+            <h2 className='font-bold hover:underline'>{props.title}</h2>
             </Link>
             <p>{props.subtitle}</p>
             <p>{props.date}</p>
