1d71107e18a3dcf9648059424bf1ba0c3410ca4a
Zenn <skyz3956@gmail.com>
2023-03-27 09:38:48 +0700

    update css style

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title>teknary</title>
+      <title>noodlenonsense's blog</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/pepe.ico" />
     </>
   )
 }
